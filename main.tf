de25e44b5110a25efbf037ab7980d1b8e2cf75f2
oleend <davidpoleen@gmail.com>
2022-02-24 14:03:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ resource "azurerm_network_interface" "main_nic_Bastion1" {
 ip_configuration {
     name                          = "Internal_Bastion1"
     subnet_id                     =  azurerm_subnet.AzureBastionSubnet.id
-    #private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.bastion_ip1.id
   }
 
