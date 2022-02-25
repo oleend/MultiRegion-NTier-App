0396f4a4ad452f0b8750bd7ca31c7e6890f84d3f
oleend <davidpoleen@gmail.com>
2022-02-25 11:18:47 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
- # backend "azurerm" {
- #  resource_group_name  = ""
- #  storage_account_name = ""
- #  container_name       = ""
- #  key                  = ""
- # }
+ backend "local"{}
 }
